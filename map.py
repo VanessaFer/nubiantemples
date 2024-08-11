9feb74d928ac449645f7fe59705a62cd5a819194
VanessaFer <52855339+VanessaFer@users.noreply.github.com>
2024-08-11 17:45:17 +0200

    Update map.py

--- map.py
+++ map.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 
 def app():
     st.header("Map of the submerged Nubian temples")
@@ -7,8 +6,7 @@ def app():
     #Aggiungere istruzioni di navigazione della mappa
 
     import streamlit.components.v1 as components
-    path_to_html = r"C:\Users\vanes\OneDrive\Desktop\mappa_templi_nubiani\mappa_templi_nubiani.html"
-    with open(path_to_html,'r') as f: 
+    with open("mappa_templi_nubiani.html",'r') as f: 
         html_data = f.read()
 
     st.components.v1.html(html_data,height=400)
