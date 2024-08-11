844006bf76cde48ca287b95b920f22a221f5d783
VanessaFer <52855339+VanessaFer@users.noreply.github.com>
2024-08-11 17:53:08 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,26 +4,31 @@ import mysql.connector
 #pip install mysql-connector-python
 
 def app():
-
-    connection = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "vanni1791",
-    database = "templi"
-)
-
-    #print("connected")
-
-    cursor = connection.cursor()
-
-    cursor.execute("Select * from personaggio")
-    data = cursor.fetchall()
-
     st.header("The Temple of Kalabsha")
     st.subheader("*A Database of all the offering scenes of the temple*")
 
-    df = pd.DataFrame(data, columns = cursor.column_names)
-    st.dataframe(df)
+    st.write("Work in progress!")
 
-    #Seleziono dati da una delle colonne della tabella che ho selezionato
-    st.write(df[df["nome_personaggio"] == "Osiris"])
+
+#     connection = mysql.connector.connect(
+#     host = "localhost",
+#     user = "root",
+#     password = "vanni1791",
+#     database = "templi"
+# )
+
+#     #print("connected")
+
+#     cursor = connection.cursor()
+
+#     cursor.execute("Select * from personaggio")
+#     data = cursor.fetchall()
+
+#     st.header("The Temple of Kalabsha")
+#     st.subheader("*A Database of all the offering scenes of the temple*")
+
+#     df = pd.DataFrame(data, columns = cursor.column_names)
+#     st.dataframe(df)
+
+#     #Seleziono dati da una delle colonne della tabella che ho selezionato
+#     st.write(df[df["nome_personaggio"] == "Osiris"])
