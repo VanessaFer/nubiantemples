ef3c34d64767ac1f4ef706bd8e713f0e2209faa5
VanessaFer <52855339+VanessaFer@users.noreply.github.com>
2024-11-17 21:28:16 +0100

    Update datavisualisation.py

--- datavisualisation.py
+++ datavisualisation.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 def app():
 
     st.header("Data Visualisation")
+    
+    components.iframe("https://public.tableau.com/views/DeitiesandKingsofthetempleofKalabsha/Dashboard1?:language=it-IT&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link", height=500)
