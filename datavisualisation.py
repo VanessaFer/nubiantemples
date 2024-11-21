e3c3aab7a6f90981999555628654b939e189f1e0
VanessaFer <52855339+VanessaFer@users.noreply.github.com>
2024-11-21 22:16:16 +0100

    Update datavisualisation.py

--- datavisualisation.py
+++ datavisualisation.py
@@ -8,7 +8,7 @@ def app():
     #components.iframe("https://public.tableau.com/views/DeitiesandKingsofthetempleofKalabsha/Dashboard1?:language=it-IT&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link", height=500)
     st.divider("A jump into the data of the temple of Kalabsha")
 
-    col1, col2 = st.columns([3,1])
+    col1, col2 = st.columns(2)
     with col1:
         st.write("""I studied the temple of Kalabsha for my MA thesis. At that time, I created a 2D representation of the walls of the temple with Rhynoceros 3D,
     using the dimensions indicated by Henri Gauthier in his publication. 
