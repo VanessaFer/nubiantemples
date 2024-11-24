b8ad0b9f7f5d82021e1d8613eed43391af57e392
Blackwood <52855339+VanessaFer@users.noreply.github.com>
2024-11-24 15:25:13 +0100

    Update databasekalabsha.py

--- databasekalabsha.py
+++ databasekalabsha.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import mysql.connector
-#pip install mysql-connector-python
+import io
 
 def app():
     st.title("The Database of the Temple of Kalabsha")
@@ -37,121 +36,45 @@ def app():
                 Henri Gauthier: from the innermost room, the Sanctuary (Cella of Gauthier) to the Pylon,
                 so KB1 is the first scene of the Cella that he describes.
 """)
-        connection = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "vanni1791",
-        database = "templi"
-    )
-
-        #print("connected")
-
-        cursor = connection.cursor()
+        
+        st.subheader("Select the scene")
         codice_scena = st.text_input("Scene code")
-        st.write("You selected:", codice_scena)
-        
-        if codice_scena == 'KB1':
-            divinita1 = f"{codice_scena}"
-            cursor.execute("""select *
-                            from personaggio p
-                            join corona c
-                            on p.codice_corona = c.codice_corona
-                            join scena s
-                            on p.codice_scena = s.codice_scena
-                            join parte par
-                            on s.codice_parte = par.codice_parte
-                            join elemento_architettonico ea
-                            on ea.codice_e_a = par.codice_e_a
-                            join ambiente a
-                            on a.codice_ambiente = ea.codice_ambiente
-                            where s.sigla_scena = 'KB1'
-                            """)
-            data = cursor.fetchall()
-            df = pd.DataFrame(data, columns = cursor.column_names)
-            df = df.loc[:,~df.columns.duplicated()]
-            df = df.loc[:,~df.columns.str.startswith('codice')]
-            st_df = st.dataframe(df, hide_index=True)
-            print(st_df)
+        st.write("You wrote:", codice_scena)
+        #codice = f"{codice_scena}"
+        df = pd.read_excel('scena_stanze_registro_titolo.xlsx')
+            # df = df.loc[:,~df.columns.duplicated()]
+        df = df.loc[:,~df.columns.str.startswith('codice')]
+        df_scene = df.loc[df['sigla_scena'] == codice_scena]
+        #df_scene = df.drop_duplicates
+        st_df_scene = st.dataframe(df_scene, hide_index=True)
+        print(st_df_scene)
 
-            if codice_scena == 'KB1':
-                cursor.execute("""select *
-                                from scena s
-                                join citata c
-                                on s.codice_scena = c.codice_scena
-                                join bibliografia_scena bs
-                                on bs.codice_bibliografia_scena = c.codice_bibliografia_scena
-                                where s.sigla_scena ='KB1'
-                                """)
-                data_biblio = cursor.fetchall()
-                df_biblio = pd.DataFrame(data_biblio, columns = cursor.column_names)
-                df_biblio = df_biblio.loc[:,~df_biblio.columns.duplicated()]
-                df_biblio = df_biblio.loc[:,~df_biblio.columns.str.startswith('codice')]
-                st.dataframe(df_biblio, hide_index=True)
+        df1 = pd.read_excel('SCENA_BIBLIOGRAFIA.xlsx')
+            # df = df.loc[:,~df.columns.duplicated()]
+        df1 = df1.loc[:,~df1.columns.str.startswith('codice')]
+        df_bibl = df1.loc[df1['sigla_scena'] == codice_scena]
+        #df_scene = df.drop_duplicates
+        st_df_bibl = st.dataframe(df_bibl, hide_index=True)
+        print(st_df_bibl)
 
-            @st.cache_data
-            def convert_df(df):
-                # IMPORTANT: Cache the conversion to prevent computation on every rerun
-                return df.to_csv().encode("utf-8")
-            csv = convert_df(df)
+        df2 = pd.read_excel('SCENA_PERSONAGGIO.xlsx')
+            # df = df.loc[:,~df.columns.duplicated()]
+        df2 = df2.loc[:,~df2.columns.str.startswith('codice')]
+        df_char = df2.loc[df2['sigla_scena'] == codice_scena]
+        #df_scene = df.drop_duplicates
+        st_df_char = st.dataframe(df_char, hide_index=True)
+        print(st_df_char)
 
-            st.download_button(
-                label="Download table as CSV",
-                data=csv,
-                file_name="kalabsha.csv",
-                mime="text/csv",)
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine = 'xlsxwriter') as writer:
 
-        #Seleziono dati da una delle colonne della tabella che ho selezionato
-        
-    with tab2:
-        st.header("Deity's name")
-        st.html("""In this tab you can do your research by the name of the deity.
-                <br>Here is a list of the deities depicted in the offering scenes
-                of the temple of Kalabsha:
-""")
-        col1, col2 = st.columns(2)
-        with col1:
-            st.html("""
-                    - unknown <br>
-                    - Amon Ra Horemahet <br>
-                    - Amon-Ra<br>
-                    - Amun<br>
-                    - Amun of Napata<br>
-                    - Amun of Primis<br>
-                    - Duat-netjer<br>
-                    - Hathor<br>
-                    - Hor-em-akh<br>et
-                    - Hor-nedj-itef<br>
-                    - Hor-pa-hred<br>
-                    - Horus<br>
-                    - Horus of Edfu<br>
-                    - Iri-hemes-nefer<br>
-                    - Isis<br>
-                    - Jj-m-hetep<br>
-                    - Khenty-mer<br>
-                    - Khnum<br>
-                    - Khonsu?<br>
-                    - Merur<br>
-""")
-        with col2:
-            st.html("""
-                    - Merur the young<br>
-                    - Min<br>
-                    - Mut<br>
-                    - Nekhbet?<br>
-                    - Nephthys<br>
-                    - Nut<br>
-                    - Osiris<br>
-                    - Pharaoh of Bigge<br>
-                    - Ptah<br>
-                    - Safekh-abui?<br>
-                    - Satet<br>
-                    - Seheter<br>
-                    - Sekhmet<br>
-                    - Shu<br>
-                    - Tanenet<br>
-                    - Tefnut<br>
-                    - Tetun<br>
-                    - Thot<br>
-                    - Uadjet<br>
-""")
-        st.html("""Select the deity you are interested in!""")
+            df_scene.to_excel(writer, sheet_name="Scene", index=False)
+            df_bibl.to_excel(writer, sheet_name="Bibliography", index=False)
+            df_char.to_excel(writer, sheet_name="Characters", index=False)
+        writer.close()
+
+        st.download_button(
+                label="Download table as Excel file",
+                data=buffer,
+                file_name="kalabsha_scene.xlsx",
+                mime="text/Excel",)
