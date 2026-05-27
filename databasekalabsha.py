7b3ec6470ed13ac726efbc45d89e77c28bca4dd2
Blackwood <52855339+VanessaFer@users.noreply.github.com>
2026-05-27 11:59:10 +0200

    Update databasekalabsha.py

--- databasekalabsha.py
+++ databasekalabsha.py
@@ -349,7 +349,7 @@ def app():
                 file_name="kalabsha_deity.xlsx",
                 mime="text/Excel",)
         
-        #from IPython.core.display import display, HTML
+       
         st.write("")
         st.write("PLATES")
         plate = st.text_input("Write the plate number")
